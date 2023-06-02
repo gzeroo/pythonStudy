c69ed1baf152d7654bae1a22890cb19236248401
Jiyoung Kim <zaz5004@naver.com>
2023-06-02 16:20:25 +0900

    20230602 파이썬 공부

---
+++ 02_1.py
@@ -0,0 +1,20 @@
+# 숫자형
+a=7;
+b=4;
+print(a%b);
+print(a//b);
+
+c="가나";
+d="다라";
+
+print(c+d);
+
+print("=" * 50);
+print("My Program");
+print("=" * 50);
+
+# f 문자열 포매팅
+name = "홍길동";
+age = 30;
+print(f'나의 이름은 {name}입니다. 나이는 {age}입니다.');
+
